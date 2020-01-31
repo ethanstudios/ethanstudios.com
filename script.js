d685dd89095292ff53007ab489650995e329caa8
ethanstudios <60440029+ethanstudios@users.noreply.github.com>
2020-01-31 19:34:32 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,95 @@
+/*THIS CODE IS PROPERTY OF ETHAN STOCKTON*/
+
+test1();
+
+function test1() {
+   
+
+  setTimeout(test2, 1500);
+
+  
+}
+
+
+function test2() {
+  document.getElementById("body").style.opacity = 1 
+
+  setTimeout(test3, 1500);
+
+  
+}
+
+function test3() {
+  document.getElementById("test2").style.left = 0
+
+  setTimeout(test3, 1500);
+
+  
+}
+
+
+
+
+function pushyMessage(){
+
+  
+   alert("Are you suer you want to untick this")
+   alert("Are you suer you want to untick this")
+   alert("Are you suer you want to untick this")
+   alert("Are you suer you want to untick this")
+   alert("Are you suer you want to untick this")
+   alert("Are you suer you want to untick this")
+   alert("Are you suer you want to untick this")
+   alert("Are you suer you want to untick this")
+   alert("Are you suer you want to untick this")
+   alert("Are you suer you want to untick this")
+   alert("Are you suer you want to untick this")
+   alert("Are you suer you want to untick this")
+   alert("Are you suer you want to untick this")
+   alert("Are you suer you want to untick this")
+   alert("Are you suer you want to untick this")
+   alert("Are you suer you want to untick this")
+   alert("Are you suer you want to untick this")
+   alert("Are you suer you want to untick this")
+   alert("Are you suer you want to untick this")
+   alert("Are you suer you want to untick this")
+   alert("Are you suer you want to untick this")
+   alert("Are you suer you want to untick this")
+   alert("Are you suer you want to untick this")
+   alert("Are you suer you want to untick this")
+   alert("Are you suer you want to untick this")
+   alert("Are you suer you want to untick this")
+   alert("Are you suer you want to untick this")
+   alert("Are you suer you want to untick this")
+   alert("Are you suer you want to untick this")
+   alert("Are you suer you want to untick this")
+   alert("Are you suer you want to untick this")
+   alert("Are you suer you want to untick this")
+   alert("Are you suer you want to untick this")
+   alert("Are you suer you want to untick this")
+   alert("Are you suer you want to untick this")
+   alert("Are you suer you want to untick this")
+   alert("Are you suer you want to untick this")
+   alert("Are you suer you want to untick this")
+   alert("Are you suer you want to untick this")
+   alert("Are you suer you want to untick this")
+   alert("Are you suer you want to untick this")
+   alert("Are you suer you want to untick this")
+   alert("Are you suer you want to untick this")
+   alert("Are you suer you want to untick this")
+   alert("Are you suer you want to untick this")
+   alert("Are you suer you want to untick this")
+   alert("Are you suer you want to untick this")
+   alert("Are you suer you want to untick this")
+   
+  
+  
+
+
+
+}
+
+
+
+
+
